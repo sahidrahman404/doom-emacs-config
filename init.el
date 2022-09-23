b14709cf0415a46f7fee672c55f3137e855ffa09
Sahid Rahman <sahidrahman404@gmail.com>
2022-09-23 21:48:28 +0700

    activate yaml support

--- init.el
+++ init.el
@@ -181,7 +181,7 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        (web               ; the tubes
         +lsp)
-       ;;yaml              ; JSON, but readable
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
